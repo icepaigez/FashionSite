5b362defa970dc7943bcdb5f5cbec7a4a43b97dc
tunde <icepaigez@gmail.com>
2021-06-09 23:04:08 +0100

    installed fontawesome lib

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class NavBar extends Component {
 	render() {
 		return(
-
+			<nav>
+				<h3>Marvy</h3>
+				<FontAwesomeIcon icon="fa-solid fa-bars" />
+				<div className="top__nav">
+					<ul>
+						<li>HOME</li>
+						<li>FASHION</li>
+						<li>WEDDING</li>
+						<li>EVENTS</li>
+						<li>THE BRAND</li>
+						<li>CONTACT US</li>
+						<li>CONSULTATION</li>
+					</ul>
+				</div>
+			</nav>
 		)
 	}
 }
