332ab5060f21444a41630cf4b9aa311544b270d0
tunde <icepaigez@gmail.com>
2021-06-11 13:24:47 +0100

    styling the navbar

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -5,13 +5,24 @@ import "./navbar.css";
 
 
 class NavBar extends Component {
+
+	toggleNavbar = () => {
+		let navElem = document.querySelector('.top__nav')
+		if (navElem.style.display === "") {
+			navElem.style.display = "block"
+		} else {
+			navElem.style.display = ""
+		}
+	}
+
 	render() {
+		
 		return(
 			<nav className="navbar">
 				<div className="header brand">
 					<h3>MARVEE</h3>
 				</div>
-				<div className="header icon">
+				<div onClick={this.toggleNavbar} className="header icon">
 					<FontAwesomeIcon className="fa__bars" icon={faBars} />
 				</div>
 				<div className="top__nav">
