dfeec9a30ff9582768cba8752a5630d919eada40
tunde <icepaigez@gmail.com>
2021-06-14 21:48:31 +0100

    setting up parallax effect

--- src/components/body/Content.js
+++ src/components/body/Content.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 import "./content.css";
-// import img1 from "../../images/marq.jpg";
+import img1 from "../../images/marq.jpg";
+import img2 from "../../images/marqe.jpg";
+import img3 from "../../images/marquez.jpg";
 
 class Content extends Component {
 	render() {
 		return(
 			<main>
-				<div className="parallax1">
-					<div className="heading">
-						Parallax
-					</div>
-					{/*<img src={img1} alt=""/>*/}
+				<div className="parallax">
+					<img src={img1} alt=""/>
+					<h2>Parallax Speed -1</h2>
 				</div>
-				<section>
+				{/*<section>
 				  <h1>Parallax Website Tutorial</h1>
 				  <h3>Learn to create a parallax scroll website</h3>
 				  <div className="two-col">
@@ -21,15 +21,15 @@ class Content extends Component {
 				  <div className="two-col">
 				    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
 				  </div>
-				</section>
-				<div className="parallax2">
-					<div className="heading-sm">
-						Fireworks
-					</div>
+				</section>*/}
+				{/*<div className="parallax">
+					<img src={img2} alt=""/>
+					<h2>Parallax Speed 0</h2>
 				</div>
-				<div>
-					<h2>3</h2>
-				</div>
+				<div className="parallax">
+					<img src={img3} alt=""/>
+					<h2>Parallax Speed 1</h2>
+				</div>*/}
 				<div>
 					<h2>4</h2>
 				</div>
