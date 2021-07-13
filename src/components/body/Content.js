82d97590dfc338b45d92245177df365db42afe7e
tunde <icepaigez@gmail.com>
2021-07-13 15:25:22 +0100

    making the navbar sticky

--- src/components/body/Content.js
+++ src/components/body/Content.js
@@ -19,7 +19,7 @@ class Content extends Component {
 					    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
 					  </div>
 				</section>
-				<div className="parallax2"></div>
+				{/*<div className="parallax2"></div>
 				<section>
 					<h1>Random Text</h1>
 					 <h3>Lorem Ipsum</h3>
@@ -51,7 +51,7 @@ class Content extends Component {
 					  <div className="two-col">
 					    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
 					  </div>
-				</section>
+				</section>*/}
 			</main>
 		)
 	}
