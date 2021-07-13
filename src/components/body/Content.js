3d0273d597378079a444b852e3fde421a199eddb
tunde <icepaigez@gmail.com>
2021-07-13 23:17:54 +0100

    styling the mid sections

--- src/components/body/Content.js
+++ src/components/body/Content.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import "./content.css";
+import img1 from "../../images/back.jpg";
+import img2 from "../../images/front.jpg";
+import img3 from "../../images/grey.jpg";
+import img4 from "../../images/low.jpg";
+import img5 from "../../images/brown.jpg";
 
 
 class Content extends Component {
@@ -11,10 +16,31 @@ class Content extends Component {
 				<section className="section__1">
 					<h1>DISCOVER</h1>
 					<div className="collections">
-						<div class="zone green">🦊</div>
-						<div class="zone red">🐰</div>
-						<div class="zone blue">🐸</div> 
-						<div class="zone yellow">🦁</div>
+						<div className="zone green">
+							<img src={img1} alt=""/>
+							<h3>BRIDE COLLECTION</h3>
+							<button>Newest styles</button>
+						</div>
+						<div className="zone red">
+							<img src={img2} alt=""/>
+							<h3>TRAD COUTURE</h3>
+							<button>Newest styles</button>
+						</div>
+						<div className="zone blue">
+							<img src={img3} alt=""/>
+							<h3>EVENING WEAR</h3>
+							<button>Newest styles</button>
+						</div> 
+						<div className="zone yellow">
+							<img src={img4} alt=""/>
+							<h3>RUNWAY</h3>
+							<button>Newest styles</button>
+						</div>
+						<div className="zone">
+							<img src={img5} alt=""/>
+							<h3>BOSS LADY</h3>
+							<button>Newest styles</button>
+						</div>
 					</div>
 				</section>
 				<div className="section s2"></div>
