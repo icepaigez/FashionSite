932ced8d391a3d914af632dbf3e9df638c89614c
tunde <icepaigez@gmail.com>
2021-07-13 10:05:48 +0100

    reworking the fashion site

---
+++ src/components/header/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+	{
+		title: 'Home',
+		url: '#',
+		cName: 'nav__links'
+	},
+	{
+		title: 'The Brand',
+		url: '#',
+		cName: 'nav__links'
+	},
+	{
+		title: 'Consultation',
+		url: '#',
+		cName: 'nav__links'
+	},
+	{
+		title: 'Media',
+		url: '#',
+		cName: 'nav__links'
+	},
+	{
+		title: 'Contact Us',
+		url: '#',
+		cName: 'nav__links'
+	}
+]
--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { MenuItems } from "./MenuItems";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import "./navbar.css";
@@ -6,7 +7,7 @@ import "./navbar.css";
 
 class NavBar extends Component {
 
-	constructor() {
+	constructor() { 
 		super()
 		this.state = {
 			navItem: [{id:1, active:true}, {id:2, active:false}, {id:3, active:false}, {id:4, active:false}, {id:5, active:false}],
@@ -14,12 +15,7 @@ class NavBar extends Component {
 	}
 
 	toggleNavbar = () => { 
-		let navElem = document.querySelector('.top__nav')
-		if (navElem.style.display === "") {
-			navElem.style.display = "block"
-		} else {
-			navElem.style.display = ""
-		}
+		console.log('this is the NavBar')
 	}
 
 	setActive = e => {
@@ -35,8 +31,19 @@ class NavBar extends Component {
 	render() {
 		const { navItem } = this.state;
 		return(
-			<nav className="navbar">
-				<div className="header brand">
+			<nav className="navbar__items">
+				<h1 className="navbar__logo">MARVEE</h1>
+				<div className="menu__icon">
+					
+				</div>
+				<ul>
+					{
+						MenuItems.map((obj, i) => {
+							return <li key={i}><a className={obj.cName} href={obj.url}>{obj.title}</a></li>
+						})
+					}
+				</ul>
+				{/*<div className="header brand">
 					<h3>MARVEE</h3>
 				</div>
 				<div onClick={this.toggleNavbar} className="header icon">
@@ -50,7 +57,7 @@ class NavBar extends Component {
 						<h5 onClick={this.setActive} className={navItem[3].active ? "active" : ""} id={navItem[3].id}>MEDIA</h5>
 						<h5 onClick={this.setActive} className={navItem[4].active ? "active" : ""} id={navItem[4].id}>CONTACT US</h5>
 					</nav>
-				</div>
+				</div>*/}
 			</nav>
 		)
 	}
