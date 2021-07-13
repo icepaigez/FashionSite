b85ea63f8bdc9b58212d16812736b37bd947b255
tunde <icepaigez@gmail.com>
2021-07-13 17:03:07 +0100

    fixing the parallax images

--- src/components/body/Content.js
+++ src/components/body/Content.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
+import { Parallax } from "react-parallax";
 import "./content.css";
-import img1 from "../../images/marq.jpg";
+import img1 from "../../images/marc.jpg";
 
 
 class Content extends Component {
@@ -8,8 +9,13 @@ class Content extends Component {
 	render() {
 		return(
 			<main>	
-				<div className="top__parallax"><img src={img1} alt=""/></div>
-				<section>
+				<Parallax bgImage={img1}>
+					<div className="parallax">
+						<div className="paral">HTML</div>
+					</div>
+				</Parallax>
+				{/*<div className="top__parallax"><img src={img1} alt=""/></div>*/}
+				{/*<section>
 					<h1>Random Text</h1>
 					 <h3>Lorem Ipsum</h3>
 					 <div className="two-col">
@@ -18,7 +24,7 @@ class Content extends Component {
 					  <div className="two-col">
 					    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
 					  </div>
-				</section>
+				</section>*/}
 				{/*<div className="parallax2"></div>
 				<section>
 					<h1>Random Text</h1>
