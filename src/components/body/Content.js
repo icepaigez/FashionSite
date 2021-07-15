21969dd9d7b646ed8d751c1857692d1f566c05eb
tunde <icepaigez@gmail.com>
2021-07-15 09:22:07 +0100

    styling the homepage

--- src/components/body/Content.js
+++ src/components/body/Content.js
@@ -43,17 +43,13 @@ class Content extends Component {
 						</div>
 					</div>
 				</section>
-				<div className="section s2"></div>
-				<section>
-					<p>
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-					</p>
-				</section>
+				
 				<div className="section s3"></div>
-				<section>
-					<p>
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-					</p>
+				<section className="section__2">
+					<h2>FOLLOW US ON <span><a href="https://www.instagram.com/marveeofficial/" target="_blank" rel="noreferrer">INSTAGRAM</a></span></h2>
+					<div className="poster">
+						hello
+					</div>
 				</section>
 			</main>
 		)
