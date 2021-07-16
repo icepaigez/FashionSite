d319034b324b0dcecb9c5fd99b70e624e3ca35db
tunde <icepaigez@gmail.com>
2021-07-16 09:00:18 +0100

    added react-router-dom for site navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from "./components/header/Navbar";
 import Content from "./components/body/Content";
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Content />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path='/'>
+            <NavBar />
+            <Content />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+export default App; 
